b72c27e8f73550defced52b00ed058cc33c0c8c5
Sunny Nagam <sunnynagam1@gmail.com>
2024-04-27 16:37:40 -0600

    Reddit multiple time frames and improved decoding

--- src/components/RedditSummary.vue
+++ src/components/RedditSummary.vue
@@ -1,42 +1,100 @@
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { useApi } from "@/useAPI.js";
 
 const { getApiKey } = useApi();
 
 const getReddit = async () => {
-  console.log(redditData.value);
-  fetch("https://l49j2lk6oh.execute-api.us-east-2.amazonaws.com/Prod/fetch", {
-    headers: {
-      "X-Api-Key": getApiKey(),
-    },
-  })
+  loading.value = true;
+  fetch(
+    `https://l49j2lk6oh.execute-api.us-east-2.amazonaws.com/Prod/fetch?timeFrame=${selectedTimeframe.value}`,
+    {
+      headers: {
+        "X-Api-Key": getApiKey(),
+      },
+    }
+  )
     .then((response) => response.json())
     .then((data) => {
       redditData.value = data;
-      console.log(redditData.value);
-      console.log(decodeHtml(redditData.value[0].posts[0].description));
+      //console.log(redditData.value[0].posts[0].description);
+      //console.log(decodeHtml(redditData.value[0].posts[0].description));
       loading.value = false;
     })
     .catch((error) => console.log("Error fetching data:", error));
 };
-const loading = ref(true);
-let redditData = ref({});
 
-getReddit();
 const decodeHtml = (html) => {
+  // find all duplicate new lines (\n\n) and replace with <br>
+  html = html.replace(/(\n\n)+/g, "<br>");
+
   var txt = document.createElement("textarea");
   txt.innerHTML = html;
-  return txt.value;
+  let parsedHtml = txt.value;
+  const imgRegex = /\.(jpg|jpeg|png|gif|bmp|webp|tiff|svg)(\?.*)?$/i;
+
+  // replace images with img tags
+  parsedHtml = parsedHtml.replace(
+    /<a\s+href="([^"]+)">([^<]+)<\/a>/g,
+    (match, href, text) => {
+      if (imgRegex.test(href)) {
+        return `<img src="${href}" alt="${text}">`;
+      } else {
+        return match; // leave non-image links unchanged
+      }
+    }
+  );
+
+  //console.log(parsedHtml);
+  return parsedHtml;
 };
+
+const redditData = ref({});
+const loading = ref(true);
+const selectedTimeframe = ref("day");
+const timeframes = ref([
+  { title: "Daily", value: "day" },
+  { title: "Weekly", value: "week" },
+  { title: "Monthly", value: "month" },
+]);
+
+getReddit();
+
+watch(selectedTimeframe, () => {
+  getReddit(); // refetch data when selectedTimeframe changes
+});
 </script>
 
 <template>
   <v-container class="mx-auto max-w-screen-md">
-    <h1 class="text-4xl font-bold mb-4">
-      <v-icon @click="console.log('hi :)')">mdi-reddit</v-icon>
-      Daily Reddit Summary
-    </h1>
+    <div class="flex flex-col items-center mb-4">
+      <h1 class="text-h2 font-bold mb-2">
+        <v-icon class="mr-2" @click="console.log('hi :)')">mdi-reddit</v-icon>
+        Reddit Summary
+      </h1>
+      <h2 class="text-subtitle-1 mb-4">
+        Summary of your favorite subreddit's top posts and comments
+      </h2>
+    </div>
+    <div class="flex justify-center mb-4">
+      <v-select
+        v-model="selectedTimeframe"
+        :items="timeframes"
+        item-title="title"
+        item-value="value"
+        label=""
+        outlined
+        density="compact"
+        variant="outlined"
+        class="w-full md:w-1/2"
+      ></v-select>
+    </div>
+    <v-progress-linear
+      v-if="loading"
+      indeterminate
+      class="mx-auto"
+      size="64"
+    ></v-progress-linear>
     <v-expansion-panels multiple popout>
       <v-expansion-panel
         v-for="(subreddit, index) in redditData"
